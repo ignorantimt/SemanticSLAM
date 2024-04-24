425671555140d2aa9c43005fd35d22459a719b20
ignorantimt <18801382209@163.com>
2024-04-24 19:46:33 +0800

    build pass (PC of robot)

--- thirdparty/remake-deps.sh
+++ thirdparty/remake-deps.sh
@@ -5,14 +5,15 @@ cmake ../opencv-3.4.10 -DCMAKE_BUILD_TYPE=Release
 make -j10
 sudo make install
 
-# cd ../..
+cd ../..
 
-# wget https://github.com/stevenlovegrove/Pangolin/archive/refs/tags/v0.6.zip
-# unzip v0.6.zip -d Pangolin
-# mkdir -p Pangolin/build && cd Pangolin/build
-# cmake ../Pangolin-0.6
-# make -j14
-# sudo make install
+wget https://github.com/stevenlovegrove/Pangolin/archive/refs/tags/v0.6.zip
+unzip v0.6.zip -d Pangolin
+mkdir -p Pangolin/build && cd Pangolin/build
+# 指定conda中的python3.10.12路径再进行编译，如果用3.11以上版本的python会报错
+cmake ../Pangolin-0.6 -DCMAKE_BUILD_TYPE=Release  -DPYTHON_EXECUTABLE=/home/rob/anaconda3/envs/semanticSLAM/bin/python
+make -j14
+sudo make install
 
-# cd ../..
+cd ../..
 

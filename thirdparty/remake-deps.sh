14278bc9504de8d92f7b3513f5fd078df7193716
ignorantimt <18801382209@163.com>
2024-04-25 16:32:21 +0800

    fix installation path of exe file and .so fule

--- thirdparty/remake-deps.sh
+++ thirdparty/remake-deps.sh
@@ -2,7 +2,7 @@ wget https://github.com/opencv/opencv/archive/3.4.10.zip
 unzip 3.4.10.zip -d opencv
 mkdir -p opencv/build && cd opencv/build
 cmake ../opencv-3.4.10 -DCMAKE_BUILD_TYPE=Release
-make -j10
+make -j20
 sudo make install
 
 cd ../..
@@ -10,8 +10,7 @@ cd ../..
 wget https://github.com/stevenlovegrove/Pangolin/archive/refs/tags/v0.6.zip
 unzip v0.6.zip -d Pangolin
 mkdir -p Pangolin/build && cd Pangolin/build
-# 指定conda中的python3.10.12路径再进行编译，如果用3.11以上版本的python会报错
-cmake ../Pangolin-0.6 -DCMAKE_BUILD_TYPE=Release  -DPYTHON_EXECUTABLE=/home/rob/anaconda3/envs/semanticSLAM/bin/python
+cmake ../Pangolin-0.6 -DCMAKE_BUILD_TYPE=Release
 make -j14
 sudo make install
 
